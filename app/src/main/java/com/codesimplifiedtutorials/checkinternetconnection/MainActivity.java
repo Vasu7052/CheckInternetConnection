2f0fcb35112c165a49dddba2bb2d0d57313658d4
androfly <androfly.official@gmail.com>
2017-03-27 22:14:02 +0530

    Checking Internet Connection In Android

---
+++ app/src/main/java/com/codesimplifiedtutorials/checkinternetconnection/MainActivity.java
@@ -0,0 +1,40 @@
+package com.codesimplifiedtutorials.checkinternetconnection;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isNetworkAvailable(MainActivity.this)){
+                    Toast.makeText(MainActivity.this, "Connected To Network", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Not Connected To Internet", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    public boolean isNetworkAvailable(final Context context) {
+        final ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
+
+}
